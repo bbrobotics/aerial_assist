f3188d1bed5e79c6a1c921595dcf5a958dddf233
Zoraver <bigz97@outlook.com>
2014-01-20 17:44:50 -0500

    Initial commit.

---
+++ src/team1517/aerialassist/RobotTemplate.java
@@ -0,0 +1,129 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package team1517.aerialassist;
+
+
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+
+import team1517.aerialassist.mecanum.MecanumDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RobotTemplate extends SimpleRobot {
+    
+    CANJaguar aF, aB, bF, bB, winchMotor;
+    Victor rotRod1, rotRod2, angle;
+    Joystick xyStick, steerStick, auxStick;
+    MecanumDrive mDrive;
+    
+    public RobotTemplate()
+    {
+        rotRod1 = new Victor(1);
+        rotRod2 = new Victor(2);
+        angle = new Victor(3);
+        
+        xyStick = new Joystick(1);
+        steerStick = new Joystick(2);
+        auxStick = new Joystick(3);
+        
+        initCANJaguars();
+        
+        mDrive = new MecanumDrive(aF, aB, bF, bB);
+    }
+    
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public void autonomous() {
+        
+    }
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        boolean exceptionFree;
+        
+        while(isOperatorControl() && isEnabled())
+        {
+            exceptionFree = mDrive.drive(xyStick.getX(), xyStick.getY(), steerStick.getTwist());
+            if(!exceptionFree)
+                initCANJaguars();
+            
+        }
+    }
+    
+    /**
+     * This function is called once each time the robot enters test mode.
+     */
+    public void test() {
+    
+    }
+    
+    
+    private boolean getHotGoal()
+    {
+        //todo stub.
+        return true;
+    }
+    
+    /**
+     * Used to initialize the CANJaguars.  It can also be called to reinitialize them if an exception is thrown.
+     * @return Success
+     */
+    private boolean initCANJaguars()
+    {
+        try
+        {
+            aF = null;
+            bF = null;
+            aB = null;
+            bB = null;
+            winchMotor = null;
+            
+            aF = new CANJaguar(1);
+            bF = new CANJaguar(2);
+            aB = new CANJaguar(3);
+            bB = new CANJaguar(4);
+            //winchMotor = new CANJaguar(5);
+            
+            aF.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+            bF.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+            aB.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+            bB.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+            //winchMotor.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+            
+            aF.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            bF.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            aB.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            bB.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            //winchMotor.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            
+            aF.configEncoderCodesPerRev(100);
+            bF.configEncoderCodesPerRev(100);
+            aB.configEncoderCodesPerRev(100);
+            bB.configEncoderCodesPerRev(100);
+            //winchMotor.configEncoderCodesPerRev(100);
+        }
+        catch(CANTimeoutException ex)
+        {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/team1517/aerialassist/mecanum/MecanumDrive.java
@@ -0,0 +1,93 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team1517.aerialassist.mecanum;
+
+import com.sun.squawk.util.MathUtils;
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+
+/**
+ *
+ * @author Zoraver
+ */
+public class MecanumDrive {
+    
+    CANJaguar aF, aB, bF, bB;
+    
+    public MecanumDrive(CANJaguar aFront, CANJaguar aBack, CANJaguar bFront, CANJaguar bBack)
+    {
+        aF = aFront;
+        aB = aBack;
+        bF = bFront;
+        bB = bBack;
+    }
+    
+    public boolean drive(double mX, double mY, double twist)
+    {
+        double x, y, magnitude, theta, highestValue, A, B, T;
+            
+            x = mX;
+            y = mY;
+            T = twist;
+            
+            magnitude = Math.sqrt(x * x + y * y);
+            theta = MathUtils.atan(y / x);
+            
+            if(x < 0)
+            {
+                theta = theta + Math.PI;
+            }
+            
+            A = Math.sqrt(2) * Math.sin(theta - 3 * Math.PI / 4);
+            B = Math.sqrt(2) * Math.cos(theta + Math.PI / 4);
+            
+            if(A > 1) A = 1;
+            else if(A < -1) A = -1;
+            
+            if(B > 1) B = 1;
+            else if(B < -1) B = -1;
+            
+            A = A * magnitude;
+            B = B * magnitude;
+            
+            if(Math.abs(A + T) > 1 || Math.abs(A - T) > 1 || Math.abs(B + T) > 1 || Math.abs(B - T) > 1)
+            {
+                highestValue = Math.abs(A + T);
+                
+                if(Math.abs(A - T) > highestValue)
+                {
+                    highestValue = Math.abs(A - T);
+                }
+                
+                if(Math.abs(B + T) > highestValue)
+                {
+                    highestValue = Math.abs(B + T);
+                }
+                
+                if(Math.abs(B - T) > highestValue)
+                {
+                    highestValue = Math.abs(B - T);
+                }
+            }
+            else
+            {
+                highestValue = 1.0;
+            }
+            
+            try
+            {
+                aF.setX((A + T) / highestValue); 
+                aB.setX((A - T) / highestValue); 
+                bF.setX((B - T) / highestValue); 
+                bB.setX((B + T) / highestValue); 
+            }
+            catch(CANTimeoutException ex)
+            {
+                ex.printStackTrace();
+                return false;
+            }
+        return true;
+    }
+}
